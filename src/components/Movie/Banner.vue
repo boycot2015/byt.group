decc4c22fe289898febe6c14ab614531e97f2efa
zhangchunhui <boycot2017@163.com>
2025-07-01 16:29:28 +0800

    优化电影横幅组件，调整加载状态处理，修复图片懒加载，更新样式以适应移动端

--- src/components/Movie/Banner.vue
+++ src/components/Movie/Banner.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { ElSkeleton, ElSkeletonItem, ElCarousel, ElCarouselItem, ElImage, ElLink } from "element-plus";
-import { ref, reactive, onMounted } from 'vue';
+import { ref, reactive, onMounted, nextTick } from 'vue';
 import { baseApiUrl } from '@/api/index.ts';
 const ElCarouselRef = ref(null);
 const state = reactive({
@@ -9,7 +9,7 @@ const state = reactive({
   cate: '13',
   type: 'cms',
   active: 0,
-  loading: false,
+  loading: true,
 });
 const isMobile = ref(false)
 onMounted(() => {
@@ -27,7 +27,9 @@ const getData = (page = 1) => {
     .then((res) => {
       state.data = res.data?.movieList.map(el => ({...el, loading: false})) || [];
       state.total = res.data.paging?.total || res.data?.total || 0;
-      state.loading = false;
+      nextTick(() => {
+          state.loading = false;
+      });
     });
 };
 getData()
@@ -36,23 +38,16 @@ getData()
 <div class="relative">
     <ElSkeleton animated :loading="state.loading" class="skeleton">
         <template #template>
-        <el-skeleton-item variant="image" class="h-[200px] rounded-md md:h-[300px] " />
+            <el-skeleton-item variant="image" :style="{height: isMobile?'160px':'400px'}" class="rounded-md" />
         </template>
         <template #default>
         <ElCarousel ref="ElCarouselRef" :height="isMobile?'160px':'400px'" :activeIndex="state.active" @change="(val) => state.active = val">
             <ElCarouselItem v-for="item in state.data" :key="item.nm" :label="isMobile?'':item.nm">
-                <a :href="`/movie/detail?type=cms&id=${item.id}`">
-                    <ElImage :src="item.videoImg" alt="" fit="fill" class="w-full rounded-md" />
+                <a :href="`/movie/detail?type=cms&id=${item.id}`" class="block w-full h-full">
+                    <ElImage v-lazy :src="item.videoImg" alt="" fit="cover" class="w-full h-full rounded-md" />
                 </a>
             </ElCarouselItem>
         </ElCarousel>
-        <!-- <div class="text absolute hidden md:block z-9999 bottom-0 text-white right-10" v-if="state.data?.length">
-            <a :href="`/movie/detail?id=${item.id}`" v-for="(item, index) in state.data" :key="item" @mouseover="ElCarouselRef.setActiveItem(index)" class="flex text-left mb-2 text-shadow-[#ccc] text-white cursor-pointer" :class="{'!text-[var(--color-primary)]': state.active == index}">
-                <ElLink type="primary" class="mr-2 text-md">{{index+1}}</ElLink>
-                <span class="mr-2">{{ item.nm }}</span>
-                <span>{{ item.status }}</span>
-            </a>
-        </div> -->
         </template>
     </ElSkeleton>
 </div>
